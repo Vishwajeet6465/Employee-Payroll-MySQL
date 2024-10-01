c89b511729bf9db6dfb66b4a44d5b43ca50dd117
Vishwajeet Bamane <vishwajeet.bamane@nmiet.edu.in>
2024-10-01 17:10:55 +0530

    Added:Created new Table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -2,3 +2,12 @@ CREATE DATABASE payroll_service;
 show databases;
 USE payroll_service;
 SELECT DATABASE();
+
+CREATE TABLE employee_payroll(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    salary Double NOT NULL,
+    start Date NOT NULL,
+    PRIMARY KEY (id)
+);
+describe employee_payroll;
